b6c200f95be5386920a37ba5fc8477a32062d76a
humphrey <writernic89@gmail.com>
2024-07-04 21:53:20 +0300

    flaskrestfulCrlab

--- server/app.py
+++ server/app.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -17,11 +14,36 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
+
+    def post(self):
+        data = request.get_json()
+        if not all(field in data for field in ['name', 'image', 'price']):
+            return jsonify({"error": "Missing data"}), 400
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return jsonify(new_plant.to_dict()), 201
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = db.session.get(Plant, id)
+        if plant is None:
+            return jsonify({"error": "Plant not found"}), 404
+        return jsonify(plant.to_dict())
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(port=5555, debug=True)
+
+
--- server/seed.py
+++ server/seed.py
@@ -4,18 +4,17 @@ from app import app
 from models import db, Plant
 
 with app.app_context():
+    db.create_all()
 
     Plant.query.delete()
 
     aloe = Plant(
-        id=1,
         name="Aloe",
         image="./images/aloe.jpg",
         price=11.50,
     )
 
     zz_plant = Plant(
-        id=2,
         name="ZZ Plant",
         image="./images/zz-plant.jpg",
         price=25.98,
